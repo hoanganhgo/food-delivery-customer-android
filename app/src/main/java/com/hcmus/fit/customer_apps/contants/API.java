5db97635a3b8ee1b979a4a386f0b80f49747163d
hoanganhgo <anhluong660@gmail.com>
2021-06-22 22:28:58 +0700

    update link release

--- app/src/main/java/com/hcmus/fit/customer_apps/contants/API.java
+++ app/src/main/java/com/hcmus/fit/customer_apps/contants/API.java
@@ -4,12 +4,12 @@ public class API {
     /**
      * 0: local,  1: release
      */
-    private static final int env = 0;
+    private static final int env = 1;
     //private static final String localHost = "http://10.0.2.2:8000";
     private static final String localHost = "http://192.168.1.7:8000";
-    private static final String releaseHost = "https://flashfood.online";
+    private static final String releaseHost = "https://api.customer.flash.bin.edu.vn";
     private static final String localSocket = "http://192.168.1.7:8010";
-    private static final String releaseSocket = "";
+    private static final String releaseSocket = "https://api.socket.flash.bin.edu.vn";
     private static final String SERVER = env == 0 ? localHost : releaseHost;
     public static final String SERVER_SOCKET = env == 0 ? localSocket : releaseSocket;
 
